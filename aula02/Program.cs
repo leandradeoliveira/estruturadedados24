f6043166a987a7f4f04c7f1fa7c674a4102b4357
Leandra de Oliveira <120530@unoesc.lan>
2024-08-28 21:46:25 -0300

    aula vetor

---
+++ aula02/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using aula02;
+
+ Person person = new Person();
+Console.WriteLine(person.Name); // null
+
+Person person1 = new Person("Mauricio gonzatto", 40);
+Console.WriteLine(person1.Name); // Mauricio gonzatto
+
+Person person2 = new Person();
+person2.Name = "doido";
+person2.Age = 70;
+Console.WriteLine(person2.Name); // doido
+
+Person person3 = new Person ()
+{ 
+    Name = "leonel Brizola", 
+    Age = 80 };
+Console.WriteLine(person3.Name); // leonel Brizola
+
+
---
+++ aula03-1-array/Program.cs
@@ -0,0 +1,40 @@
+﻿//  array unidimensional
+
+int[] numbers;
+numbers = new int[5];
+numbers[0] = 1;
+numbers[1] = 2;
+numbers[2] = 3;
+numbers[3] = 4; 
+numbers[4] = 5;
+
+// declaracao inicializada
+string[] names = { "Eu", "Tu", "Ele", "Nos", "Vos", "Eles"};
+
+// vetor conm o nome dos meses
+string[]  months = new string[12];
+
+for(int i = 1; i <= 12; i++)
+{
+    DateTime firstDay = new DateTime(DateTime.Now.Year, i, 1);
+    string name = firstDay.ToString("MMMM");
+
+    months[i-1] = name;
+}
+
+foreach(string m in months) 
+{
+    Console.WriteLine(m);
+}
+
+
+
+
+
+
+
+
+
+
+
+
